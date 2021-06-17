86d8d4c051c458b682a2a11e1b2962b081755815
Bermet <71565678+BermetBatieva@users.noreply.github.com>
2021-06-18 03:03:04 +0600

    Delete Week7/Part1/product directory

--- Week7/Part1/product/src/main/java/com/example/product/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.product.repository;
-
-import com.example.product.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
-}
--- Week7/Part1/product/src/main/java/com/example/product/repository/SaleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.product.repository;
-
-import com.example.product.entity.Sale;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaleRepository extends JpaRepository<Sale,Integer> {
-}
